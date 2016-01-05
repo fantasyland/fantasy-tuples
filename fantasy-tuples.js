090331591b8edd3cb61cb90d99b99b372d633033
SimonRichardson <stickupkid@gmail.com>
2016-01-05 17:55:52 +0000

    Tuples

--- fantasy-tuples.js
+++ fantasy-tuples.js
@@ -1,4 +1,10 @@
-const Tuples = require('./src/tuples');
+const nested = require('./src/nested');
+const tuples = require('./src/tuples');
 
 if (typeof module != 'undefined')
-    module.exports = Tuples;
+    module.exports = { nested, tuples, 
+                       Tuple2: tuples.Tuple2, 
+                       Tuple3: tuples.Tuple3, 
+                       Tuple4: tuples.Tuple4, 
+                       Tuple5: tuples.Tuple5
+                     };
