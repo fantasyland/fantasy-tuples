b9355743307a45d17ea061d758d3ed0c40535c51
SimonRichardson <stickupkid@gmail.com>
2016-01-26 15:55:01 +0000

    Make sure we add Tuple

--- fantasy-tuples.js
+++ fantasy-tuples.js
@@ -2,6 +2,7 @@ const nested = require('./src/nested');
 const tuples = require('./src/tuples');
 
 module.exports = { nested
+                 , Tuple: nested.Tuple
                  , tuples
                  , Tuple2: tuples.Tuple2
                  , Tuple3: tuples.Tuple3 
